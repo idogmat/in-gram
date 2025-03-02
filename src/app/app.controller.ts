4ee94b5dbc4584c6e7f2e9a3dd87bffcf5b7a9d5
idogmat <idogmat@gmail.com>
2025-03-02 14:30:32 +0300

    feature: new route

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -23,4 +23,9 @@ export class AppController {
   ): Promise<string> {
     throw new ForbiddenException()
   }
+  @Get('/error2')
+  async getForbidden2(
+  ): Promise<string> {
+    throw new ForbiddenException({ message: '2' })
+  }
 }
